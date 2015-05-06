ed73a738f58ca25c561639cc8295da9c6d4be464
HERAULT-MARTIN Pierre <HicksCorp@GMail.com>
2015-05-06 21:23:24 +0100

    Refactors minor things, and adds the ability to directly query for chunks of data.

--- ext/osimplex/generator.c
+++ ext/osimplex/generator.c
@@ -1,6 +1,11 @@
 #include "generator.h"
 #include "open-simplex-noise.h"
 
+#define   SELF2GENERATOR() \
+            OSimplexGenerator   *gen;   \
+            Data_Get_Struct(self, OSimplexGenerator, gen)
+
+
 #define max(a,b) \
    ({ __typeof__ (a) _a = (a); \
        __typeof__ (b) _b = (b); \
@@ -14,11 +19,15 @@ void Init_osimplex () {
   VALUE clsModule     = rb_define_module("OSimplex");
   VALUE clsGenerator  = rb_define_class_under(clsModule, "Generator", rb_cObject);
 
-  rb_define_alloc_func( clsGenerator,               OSimplex_Generator_allocate);
-  rb_define_method(     clsGenerator, "initialize", OSimplex_Generator_init, -1);
-  rb_define_method(     clsGenerator, "get_2d",     OSimplex_Generator_get_2d, 2);
-  rb_define_method(     clsGenerator, "get_3d",     OSimplex_Generator_get_3d, 3);
-  rb_define_method(     clsGenerator, "get_4d",     OSimplex_Generator_get_3d, 4);
+  rb_define_alloc_func( clsGenerator,             OSimplex_Generator_allocate);
+  rb_define_method(clsGenerator,  "initialize",   OSimplex_Generator_init, -1);
+  rb_define_method(clsGenerator,  "get_2d",       OSimplex_Generator_get_2d, 2);
+  rb_define_method(clsGenerator,  "get_3d",       OSimplex_Generator_get_3d, 3);
+  rb_define_method(clsGenerator,  "get_4d",       OSimplex_Generator_get_3d, 4);
+
+  rb_define_method(clsGenerator,  "get_2d_chunk", OSimplex_Generator_get_2d_chunk, 4);
+  rb_define_method(clsGenerator,  "get_3d_chunk", OSimplex_Generator_get_3d_chunk, 5);
+  rb_define_method(clsGenerator,  "get_4d_chunk", OSimplex_Generator_get_4d_chunk, 6);
 }
 
 // Allocate a new Generator object instance.
@@ -29,8 +38,7 @@ VALUE OSimplex_Generator_allocate (VALUE klass) {
 }
 // Initializes a newly created Generator object instance.
 VALUE OSimplex_Generator_init (const int argc, const VALUE *argv, const VALUE self) {
-  OSimplexGenerator* gen;
-  Data_Get_Struct(self, OSimplexGenerator, gen);
+  SELF2GENERATOR();
 
   VALUE   seed    = UINT2NUM(0);
   VALUE   scale   = rb_float_new(1.0);
@@ -56,24 +64,75 @@ inline double normalize (const double n, const OSimplexGenerator *gen) {
   // Normalize between 0 and 1, apply contrast and brightness, then stay within 0..1 range.
   return max( min( gen->alpha * ( ( ( 1.0 + n ) / 2.0 ) + gen->beta ), 1.0 ), 0.0 );
 }
-VALUE OSimplex_Generator_get_2d (VALUE self, VALUE x, VALUE y) {
-  OSimplexGenerator* gen;
-  Data_Get_Struct(self, OSimplexGenerator, gen);
-
+VALUE OSimplex_Generator_get_2d (const VALUE self, const VALUE x, const VALUE y) {
+  SELF2GENERATOR();
   double  ret   = open_simplex_noise2( gen->context, NUM2DBL(x) / gen->scale, NUM2DBL(y) / gen->scale );
   return  rb_float_new( normalize(ret, gen ) );
 }
-VALUE OSimplex_Generator_get_3d (VALUE self, VALUE x, VALUE y, VALUE z) {
-  OSimplexGenerator* gen;
-  Data_Get_Struct(self, OSimplexGenerator, gen);
+VALUE OSimplex_Generator_get_3d (const VALUE self, const VALUE x, const VALUE y, const VALUE z) {
+  SELF2GENERATOR();
 
   double  ret   = open_simplex_noise3( gen->context, NUM2DBL(x) / gen->scale, NUM2DBL(y) / gen->scale, NUM2DBL(z) / gen->scale );
   return  rb_float_new( normalize(ret, gen ) );
 }
-VALUE OSimplex_Generator_get_4d (VALUE self, VALUE x, VALUE y, VALUE z, VALUE w) {
-  OSimplexGenerator* gen;
-  Data_Get_Struct(self, OSimplexGenerator, gen);
+VALUE OSimplex_Generator_get_4d (const VALUE self, const VALUE x, const VALUE y, const VALUE z, const VALUE w) {
+  SELF2GENERATOR();
 
   double  ret   = open_simplex_noise4( gen->context, NUM2DBL(x) / gen->scale, NUM2DBL(y) / gen->scale, NUM2DBL(z) / gen->scale, NUM2DBL(w) / gen->scale );
   return  rb_float_new( normalize(ret, gen ) );
+}
+
+VALUE OSimplex_Generator_get_2d_chunk (const VALUE self, const VALUE x, const VALUE y, const VALUE width, const VALUE height) {
+  SELF2GENERATOR();
+  const int           xStart    = NUM2INT(x);
+  const int           xEnd      = xStart + NUM2INT(width);
+  const int           yStart    = NUM2INT(y);
+  const int           yEnd      = yStart + NUM2INT(height);
+
+  VALUE               result    = rb_ary_new();
+  for (int j=yStart; j<yEnd; ++j) {
+    const double        jVal      = j / gen->scale;
+    for (int i=xStart; i<xEnd; ++i) {
+      double            val       = normalize( open_simplex_noise2( gen->context, i / gen->scale, jVal ), gen);
+      rb_ary_push(result, rb_float_new(val));
+    }
+  }
+  return result;
+}
+VALUE OSimplex_Generator_get_3d_chunk (const VALUE self, const VALUE x, const VALUE y, const VALUE z, const VALUE width, const VALUE height) {
+  SELF2GENERATOR();
+  const int           xStart    = NUM2INT(x);
+  const int           xEnd      = xStart + NUM2INT(width);
+  const int           yStart    = NUM2INT(y);
+  const int           yEnd      = yStart + NUM2INT(height);
+  const double        zVal      = NUM2DBL(z) / gen->scale;
+
+  VALUE               result    = rb_ary_new();
+  for (int j=yStart; j<yEnd; ++j) {
+    const double        jVal      = j / gen->scale;
+    for (int i=xStart; i<xEnd; ++i) {
+      double            val       = normalize( open_simplex_noise3( gen->context, i / gen->scale, jVal, zVal ), gen);
+      rb_ary_push(result, rb_float_new(val));
+    }
+  }
+  return result;
+}
+VALUE OSimplex_Generator_get_4d_chunk (const VALUE self, const VALUE x, const VALUE y, const VALUE z, const VALUE w, const VALUE width, const VALUE height) {
+  SELF2GENERATOR();
+  const int           xStart    = NUM2INT(x);
+  const int           xEnd      = xStart + NUM2INT(width);
+  const int           yStart    = NUM2INT(y);
+  const int           yEnd      = yStart + NUM2INT(height);
+  const double        zVal      = NUM2DBL(z) / gen->scale;
+  const double        wVal      = NUM2DBL(w) / gen->scale;
+
+  VALUE               result    = rb_ary_new();
+  for (int j=yStart; j<yEnd; ++j) {
+    const double        jVal      = j / gen->scale;
+    for (int i=xStart; i<xEnd; ++i) {
+      double            val       = normalize( open_simplex_noise4( gen->context, i / gen->scale, jVal, zVal, wVal ), gen);
+      rb_ary_push(result, rb_float_new(val));
+    }
+  }
+  return result;
 }
--- ext/osimplex/generator.h
+++ ext/osimplex/generator.h
@@ -10,13 +10,17 @@ typedef struct _OSimpleGenerator {
   struct osn_context *context;
 } OSimplexGenerator;
 
-void    Init_osimplex               ();
-VALUE   OSimplex_Generator_allocate (VALUE klass);
-VALUE   OSimplex_Generator_init     (const int argc, const VALUE *argv, const VALUE self);
-void    OSimplex_Generator_free     (OSimplexGenerator *gen);
+void    Init_osimplex                   ();
+VALUE   OSimplex_Generator_allocate     (VALUE klass);
+VALUE   OSimplex_Generator_init         (const int argc, const VALUE *argv, const VALUE self);
+void    OSimplex_Generator_free         (OSimplexGenerator *gen);
 
-VALUE   OSimplex_Generator_get_2d   (VALUE self, VALUE x, VALUE y);
-VALUE   OSimplex_Generator_get_3d   (VALUE self, VALUE x, VALUE y, VALUE z);
-VALUE   OSimplex_Generator_get_4d   (VALUE self, VALUE x, VALUE y, VALUE z, VALUE w);
+VALUE   OSimplex_Generator_get_2d       (const VALUE self, const VALUE x, const VALUE y);
+VALUE   OSimplex_Generator_get_3d       (const VALUE self, const VALUE x, const VALUE y, const VALUE z);
+VALUE   OSimplex_Generator_get_4d       (const VALUE self, const VALUE x, const VALUE y, const VALUE z, const VALUE w);
+
+VALUE   OSimplex_Generator_get_2d_chunk (const VALUE self, const VALUE x, const VALUE y, const VALUE width, const VALUE height);
+VALUE   OSimplex_Generator_get_3d_chunk (const VALUE self, const VALUE x, const VALUE y, const VALUE z, const VALUE width, const VALUE height);
+VALUE   OSimplex_Generator_get_4d_chunk (const VALUE self, const VALUE x, const VALUE y, const VALUE z, const VALUE w, const VALUE width, const VALUE height);
 
 #endif
